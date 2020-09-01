861318efc34ab086cf422a9d81a7d598ae812c4f
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2020-09-01 21:14:59 +0200

    add calculator buttons

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,35 @@ const PageComponent = gg.component(() => {
         <h1>Calculator App</h1>
         <p>Apply one basic arithmetic operation at a time to two numbers.</p>
         <div>3 + 4</div>
+        <div>
+          <button>AC</button>
+          <button>C</button>
+          <button>ANS</button>
+          <button>{"&div;"}</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>{"&times;"}</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+        <div>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </body>
     </html>
   );
