e49bf716b72d9276b7f4f4a525cd885c175a43d5
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2021-01-08 14:45:12 +0100

    export SetStateHandler type from deps

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
 export { default as gg } from "https://gg.thomasheyenbrock.com/code/<gg-token>/gg@0.1.0/mod.ts";
+export type { SetStateHandler } from "https://gg.thomasheyenbrock.com/code/<gg-token>/gg@0.1.0/mod.ts";
